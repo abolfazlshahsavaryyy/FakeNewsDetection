ad4831bc831b371e86530981f00d08b1e11d3a4e
Abolfazl <abolfazlshahsavaryyy@gmail.com>
2025-06-27 19:15:34 +0330

    feat: runable the main from src and set the src as root directory

--- src/main.py
+++ src/main.py
@@ -4,9 +4,13 @@ from src.visualization.eda import scatter_featurs
 from src.features.split_data import split_data
 
 def main():
+    print("src main.py has been run :)")
+    print("Read data")
     df=read_data("data/Fake.csv",'data/True.csv')
+    print("clean data")
     clean_df=clean_data(df)
-    scatter_featurs(clean_data)
+    print("scatter plot the featurs")
+    scatter_featurs(clean_df)
     x_train,x_test,x_valid,y_train,y_test,y_valid=split_data(clean_df)
 
 
