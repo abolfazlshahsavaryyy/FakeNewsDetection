73da9b1267b0468e66e7b6ec84ddcf605b7bfbe0
Abolfazl <abolfazlshahsavaryyy@gmail.com>
2025-06-28 13:41:52 +0330

    fix: fix bug in passing params to models

--- src/models/train_model/LogisticRegression_model.py
+++ src/models/train_model/LogisticRegression_model.py
@@ -1,7 +1,7 @@
 from sklearn.linear_model import LogisticRegression
 
 def LogisticRegression_model(X_train,y_train,hyperparameter):
-    model=LogisticRegression(hyperparameter)
+    model=LogisticRegression(** hyperparameter)
     model.fit(X_train,y_train)
 
     return model
