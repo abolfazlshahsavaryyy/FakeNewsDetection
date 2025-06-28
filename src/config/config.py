11514eb4d5d24eaaec7ccb4d08eda45e94d702f0
Abolfazl <abolfazlshahsavaryyy@gmail.com>
2025-06-28 13:41:12 +0330

    feat: add models param in config

--- src/config/config.py
+++ src/config/config.py
@@ -16,5 +16,18 @@ class Settings:
 
     # Logging, DB, or CORS config can go here too
     # DATABASE_URL: str = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+    PARAM_GRID_LOGISTIC = {
+    "C": 1.0,
+    "penalty": "l2",
+    "solver": "liblinear",
+    "max_iter": 100
+    }
+    logistic_regression_paramater={
+        'max_iter':1000
+    }
+    svc_parameter={
+        'max_iter':1000,
+        'C':0.01
+    }
 
 settings = Settings()
